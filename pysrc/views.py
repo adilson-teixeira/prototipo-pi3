d12762628ca609339bd16900662007d45131169a
proj-int01 <adilson.monte.alto@gmail.com>
2022-09-07 10:29:13 -0300

    test

--- pysrc/views.py
+++ pysrc/views.py
@@ -4,4 +4,4 @@ from http.client import HTTPResponse
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Teste de apresentação da página')
+    return HttpResponse('Teste de apresentação')
