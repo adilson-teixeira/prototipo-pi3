409fa64e74392cae0fb24d296ce4493cd3ed805d
proj-int01 <adilson.monte.alto@gmail.com>
2022-09-07 09:51:25 -0300

    test3

--- pysrc/settings.py
+++ pysrc/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-4w9&tf$boc!60ooa^l@14^^hg94x0+qd21p)oz)zwptv+5@qyx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEBUG = config('DEBUG', cast=bool, default=False)
