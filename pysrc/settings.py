b1c76505dc7f61f177c881917ab048a19d64bfb5
proj-int01 <adilson.monte.alto@gmail.com>
2022-09-08 03:46:29 -0300

    troca bd

--- pysrc/settings.py
+++ pysrc/settings.py
@@ -84,11 +84,21 @@ WSGI_APPLICATION = 'pysrc.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django3',
+        'USER': 'django',
+        'PASSWORD': 'Django2',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
