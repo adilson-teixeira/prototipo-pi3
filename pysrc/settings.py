2d22050535b2ffa511538af554405a29e43cef05
proj-int01 <adilson.monte.alto@gmail.com>
2022-09-08 10:16:37 -0300

    test secret_key

--- pysrc/settings.py
+++ pysrc/settings.py
@@ -26,8 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-#SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY = 'django-insecure-lmfi&a*z%=30yf8r&mkr$s=(o+4su_m(y#dlguzyuj2a4uf2lt'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEBUG = config('DEBUG', cast=bool, default=True)
